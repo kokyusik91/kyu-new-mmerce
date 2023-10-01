80317349fd79d16d74e23600ece6e40a859d8563
kyusikko <kokyusik91@naver.com>
2023-10-01 19:11:36 +0900

    ✨ 카테고리 fetching 후 보여주기

---
+++ components/NavBar.tsx
@@ -0,0 +1,23 @@
+import Container from '@/components/ui/container';
+import Link from 'next/link';
+import MainNav from '@/components/MainNav';
+import getCategories from '@/actions/get-categories';
+
+export const revalidate = 0;
+
+export default async function NavBar() {
+  const categories = await getCategories();
+
+  return (
+    <div className='border-b'>
+      <Container>
+        <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center'>
+          <Link href='/' className='ml-4 flex lg:ml-0 gap-x-2'>
+            <p className='font-bold text-xl'>STORE</p>
+          </Link>
+          <MainNav data={categories} />
+        </div>
+      </Container>
+    </div>
+  );
+}
