861cec74c87ef1926198530c1b31f6fa14395dc0
kyusikko <kokyusik91@naver.com>
2023-10-01 19:42:28 +0900

    ✨ 특정 아이디에 대한 빌보드 가져오기

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,16 @@
-export default function HomePage() {
-  return <div>헬로우!!</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const billboard = await getBillboard('834c7420-7dc2-43fa-bd02-0a036933ff53');
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 }
