7746509e0403358198596e1fa576322633978a40
kyusikko <kokyusik91@naver.com>
2023-10-01 19:38:10 +0900

    ✨ 공통 Navbar 만들기

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,6 +2,7 @@ import Container from '@/components/ui/container';
 import Link from 'next/link';
 import MainNav from '@/components/MainNav';
 import getCategories from '@/actions/get-categories';
+import NavbarActions from '@/components/NavbarActions';
 
 export const revalidate = 0;
 
@@ -16,6 +17,7 @@ export default async function NavBar() {
             <p className='font-bold text-xl'>STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
