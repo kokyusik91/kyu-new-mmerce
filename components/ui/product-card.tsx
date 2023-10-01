4477140ee4450a9bbd7423b64c3b29254b9640c0
kyusikko <kokyusik91@naver.com>
2023-10-01 21:10:45 +0900

    ✨ 프로덕트 카드 눌렀을때 상세페이지 이동 및 상세페이지 개발

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,15 +5,22 @@ import Image from 'next/image';
 import IconButton from '@/components/ui/icon-button';
 import { Expand, ShoppingCart } from 'lucide-react';
 import Currency from '@/components/ui/Currency';
+import { useRouter } from 'next/navigation';
 
 interface ProductCardProps {
   data: Product;
 }
 
 export default function ProductCard({ data }: ProductCardProps) {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
   return (
-    <div className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
-      {}
+    <div
+      onClick={handleClick}
+      className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'
+    >
       <div className='aspect-square rounded-xl bg-gray-100 relative'>
         <Image
           src={data?.images?.[0]?.url}
