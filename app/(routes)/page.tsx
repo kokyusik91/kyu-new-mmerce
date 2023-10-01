6c0c204cf1e374967aa945a704479ad115250032
kyusikko <kokyusik91@naver.com>
2023-10-01 20:13:49 +0900

    🏷️ get-products api 만들기 + 페이지 컴포넌트에서 fetch하기

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import getBillboard from '@/actions/get-billboard';
 import Billboard from '@/components/billboard';
 import Container from '@/components/ui/container';
+import getProducts from '@/actions/get-products';
+import ProductList from '@/components/ProductList';
 
 export const revalidate = 0;
 
 export default async function HomePage() {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard('834c7420-7dc2-43fa-bd02-0a036933ff53');
   return (
     <Container>
       <div className='space-y-10 pb-10'>
         <Billboard data={billboard} />
+      </div>
+      <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+        <ProductList title='Featured Products' items={products} />
       </div>
     </Container>
   );
